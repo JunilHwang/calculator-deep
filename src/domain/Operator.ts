f92c891cc3d44161da322e3a52bff6e99d977ab2
junilHwang <junil.h@kakao.com>
2022-03-24 23:21:24 +0900

    feat: Operator를 static으로 사용할 수 있는 메소드 추가

--- src/domain/Operator.ts
+++ src/domain/Operator.ts
@@ -1,25 +1,29 @@
 import {
   InvalidOperatorConstructException,
   InvalidOperatorParamException,
+  NotFoundOperatorException,
 } from "./exceptions";
 
 export class Operator {
+  private static values: Record<string, Operator> = {};
+
   public static ADD = new Operator("+", (x, y) => x + y);
   public static SUBTRACT = new Operator("-", (x, y) => x - y);
   public static MULTIPLY = new Operator("X", (x, y) => x * y);
   public static DIVISION = new Operator("/", (x, y) => x / y);
 
   constructor(
-    private readonly _operator: string,
+    private readonly operator: string,
     private readonly _calculate: (x: number, y: number) => number
   ) {
-    if (_operator === undefined || _calculate === undefined) {
+    if (operator === undefined || _calculate === undefined) {
       throw new InvalidOperatorConstructException();
     }
+    Operator.values[operator] = this;
   }
 
-  public get operator() {
-    return this._operator;
+  public get symbol() {
+    return this.operator;
   }
 
   public calculate(x: number, y: number) {
@@ -27,5 +31,13 @@ export class Operator {
       throw new InvalidOperatorParamException();
     }
     return this._calculate(x, y);
+  }
+
+  public static calculate(symbol: string, x: number, y: number) {
+    const operator: Operator = this.values[symbol];
+    if (operator === undefined) {
+      throw new NotFoundOperatorException();
+    }
+    return this.values[symbol].calculate(x, y);
   }
 }
