f7930e030b7fd9ae74c197a53e7a35f6dbe923d2
junilHwang <junil.h@kakao.com>
2022-03-24 21:48:57 +0900

    chore: vitest 추가 및 환경설정 완료

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,19 @@
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
   plugins: ["@typescript-eslint"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier",
+    "plugin:prettier/recommended",
   ],
 };
