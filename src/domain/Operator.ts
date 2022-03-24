a199373d5fc7d5e9a63d2be39e1d5f3b75331823
junilHwang <junil.h@kakao.com>
2022-03-24 22:10:28 +0900

    feat: Operator class 작성 완료

---
+++ src/domain/Operator.ts
@@ -0,0 +1,19 @@
+export class Operator {
+  public static PLUS = new Operator("+", (x, y) => x + y);
+  public static SUBTRACT = new Operator("-", (x, y) => x - y);
+  public static MULTIPLY = new Operator("X", (x, y) => x * y);
+  public static DIVISION = new Operator("/", (x, y) => x / y);
+
+  constructor(
+    private readonly _operator: string,
+    private readonly _calculate: (x: number, y: number) => number
+  ) {}
+
+  public get operator() {
+    return this._operator;
+  }
+
+  public get calculate() {
+    return this._calculate;
+  }
+}
--- test/domain/Operator.test.ts
+++ test/domain/Operator.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { Operator } from "../../src/domain/Operator";
+import { Operator } from "../../src/domain";
 
 describe("연산자 테스트", () => {
   const x = 10;
@@ -22,41 +22,41 @@ describe("연산자 테스트", () => {
 
   describe("덧셈, 뺄셈, 나눗셈, 곱셈을 지원", () => {
     it("덧셈", () => {
-      expect(Operator.PLUS.calulate(x, y)).toBe(x + y);
+      expect(Operator.PLUS.calculate(x, y)).toBe(x + y);
     });
     it("뺄셈", () => {
-      expect(Operator.SUBTRACT.calulate(x, y)).toBe(x - y);
+      expect(Operator.SUBTRACT.calculate(x, y)).toBe(x - y);
     });
     it("나눗셈", () => {
-      expect(Operator.DIVISION.calulate(x, y)).toBe(x / y);
+      expect(Operator.DIVISION.calculate(x, y)).toBe(x / y);
     });
     it("곱셈", () => {
-      expect(Operator.MULTIPLY.calulate(x, y)).toBe(x * y);
+      expect(Operator.MULTIPLY.calculate(x, y)).toBe(x * y);
     });
   });
 
   describe("연산자 확장", () => {
     it("제곱 연산", () => {
       const operator = new Operator("^", (x, y) => x ** y);
-      expect(operator.calulate(2, 0)).toBe(1);
-      expect(operator.calulate(2, 1)).toBe(2);
-      expect(operator.calulate(2, 2)).toBe(2 * 2);
-      expect(operator.calulate(2, 3)).toBe(2 * 2 * 2);
-      expect(operator.calulate(2, 4)).toBe(2 * 2 * 2 * 2);
-      expect(operator.calulate(2, 5)).toBe(2 * 2 * 2 * 2 * 2);
-      expect(operator.calulate(2, 6)).toBe(2 * 2 * 2 * 2 * 2 * 2);
-      expect(operator.calulate(2, 7)).toBe(2 * 2 * 2 * 2 * 2 * 2 * 2);
-      expect(operator.calulate(2, 8)).toBe(2 * 2 * 2 * 2 * 2 * 2 * 2 * 2);
+      expect(operator.calculate(2, 0)).toBe(1);
+      expect(operator.calculate(2, 1)).toBe(2);
+      expect(operator.calculate(2, 2)).toBe(2 * 2);
+      expect(operator.calculate(2, 3)).toBe(2 * 2 * 2);
+      expect(operator.calculate(2, 4)).toBe(2 * 2 * 2 * 2);
+      expect(operator.calculate(2, 5)).toBe(2 * 2 * 2 * 2 * 2);
+      expect(operator.calculate(2, 6)).toBe(2 * 2 * 2 * 2 * 2 * 2);
+      expect(operator.calculate(2, 7)).toBe(2 * 2 * 2 * 2 * 2 * 2 * 2);
+      expect(operator.calculate(2, 8)).toBe(2 * 2 * 2 * 2 * 2 * 2 * 2 * 2);
     });
 
     it("나머지 연산", () => {
       const operator = new Operator("%", (x, y) => x % y);
-      expect(operator.calulate(3, 1)).toBe(1);
-      expect(operator.calulate(3, 2)).toBe(2);
-      expect(operator.calulate(3, 3)).toBe(0);
-      expect(operator.calulate(3, 4)).toBe(1);
-      expect(operator.calulate(3, 5)).toBe(2);
-      expect(operator.calulate(3, 6)).toBe(0);
+      expect(operator.calculate(1, 3)).toBe(1);
+      expect(operator.calculate(2, 3)).toBe(2);
+      expect(operator.calculate(3, 3)).toBe(0);
+      expect(operator.calculate(4, 3)).toBe(1);
+      expect(operator.calculate(5, 3)).toBe(2);
+      expect(operator.calculate(6, 3)).toBe(0);
     });
   });
 });
